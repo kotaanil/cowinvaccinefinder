d0afc65705e652c323059bbc8e3d866e1ef97a86
Durga Prasad Reddy Vennapusa <durgaprasad.vennapusa@gmail.com>
2021-05-14 22:29:44 +0530

    Add project files.

---
+++ Source/DTO/GenerateMobileOTPResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class GenerateMobileOTPResponseDTO
+    {
+        public string txnId { get; set; }
+    }
+}
---
+++ Source/DTO/ValidateMobileOTPResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class ValidateMobileOTPResponseDTO
+    {
+        public string token { get; set; }
+        public string isNewAccount { get; set; }
+    }
+}
