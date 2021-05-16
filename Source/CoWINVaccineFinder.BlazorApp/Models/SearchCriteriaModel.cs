1d40ae5526835aedc74d65840162d681b4570ec4
Durga Prasad Reddy Vennapusa <durgaprasad.vennapusa@gmail.com>
2021-05-16 22:45:27 +0530

    added partial ui

---
+++ Source/CoWINVaccineFinder.BlazorApp/Models/SearchCriteriaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoWINVaccineFinder.BlazorApp.Models
+{
+    public class SearchCriteriaModel
+    {
+        public string State { get; set; }
+        public string District { get; set; }
+    }
+}
